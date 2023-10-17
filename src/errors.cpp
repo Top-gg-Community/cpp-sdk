13c1bf7c5578b7a7151d69dd61f686f65bbfe44e
Mohammed Keyvanzadeh <mohammadkeyvanzade94@gmail.com>
2023-10-17 04:18:10 +0330

    *: format all files using Clang-Format
    Format all C++ files to the Google preset style using Clang-Format.

--- src/errors.cpp
+++ src/errors.cpp
@@ -3,16 +3,9 @@
 using namespace topgg;
 
 TopggAPIError::TopggAPIError(const std::string& message)
-    : std::runtime_error(message)
-{
-}
+    : std::runtime_error(message) {}
 
 RateLimitError::RateLimitError(const std::string& message, int retryAfter)
-    : TopggAPIError(message), m_retryAfter(retryAfter)
-{
-}
+    : TopggAPIError(message), m_retryAfter(retryAfter) {}
 
-int RateLimitError::getRetryAfter() const
-{
-    return m_retryAfter;
-}
+int RateLimitError::getRetryAfter() const { return m_retryAfter; }
